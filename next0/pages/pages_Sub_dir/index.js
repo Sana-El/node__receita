712f90eb7af9d0716112331ec9239e798e59c42a
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2022-06-25 17:13:19 -0300

    Update index.js

--- next0/pages/pages_Sub_dir/index.js
+++ next0/pages/pages_Sub_dir/index.js
@@ -1,17 +1,22 @@
-export default function Home(){
+export default function principal(){
+    return(
+        <div>
+            <h1>
+                Nova pagina
+            </h1>
 
-    return (
- 
-       <div>
- 
-          <h2>
- 
-            Viva Santana!
- 
-          </h2>
- 
-       </div>
- 
+            <Mariaprea texto = "Morreu Maria préa..."/>
+        </div>
     )
- 
- }
+}
+
+export function Mariaprea({texto}){
+    return(
+
+        <h2>
+        
+           {texto}
+
+        </h2>
+    )
+}
