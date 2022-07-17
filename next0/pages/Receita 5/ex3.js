441c16ddb9b1940cb5149a745c20e7be703c87e7
Samuel Gutemberg <71667058+Sana-El@users.noreply.github.com>
2022-07-17 13:40:54 -0300

    Create ex3.js

---
+++ next0/pages/Receita 5/ex3.js
@@ -0,0 +1,75 @@
+import {useState} from 'react'
+
+export default function Movies3(){
+
+    const [url, setUrl] = useState('')
+    const [data, setData] = useState('')
+
+    const onClickHandler = async (e) => {
+        const link = e.target.id
+
+        e.preventDefault()
+        if (url === ''){
+            setUrl(link)
+            const result = await theFetcher(link)
+            console.log(result)
+            setData(result)
+            return Movies3
+        }else {
+            setUrl('')
+            setData('')
+            return Movies3
+        }
+
+        
+
+    }
+
+    return (
+
+        <div>
+            <TheLink url={url} link={'`https://www.omdbapi.com/?apikey=8ba47b1d&s=bagdad'} handler={onClickHandler}/>
+            <TheLink url={url} link={'`https://www.omdbapi.com/?apikey=8ba47b1d&s=2012'} handler={onClickHandler} />
+            <TheMovies data={data}/>
+        </div>
+
+    )
+
+}
+
+async function theFetcher(url) {
+
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+
+}
+
+export function TheMovies({data}){
+
+    if(data == '') return <div></div>
+
+    return (
+
+        <div>
+            { data.Search.map( (m, i) => <div key={i}>{m.Title} --- {m.Year}</div>  ) }            
+
+        </div>
+
+    )
+
+}
+
+export function TheLink({url, handler, link}){    
+
+    return (
+
+        <div>
+
+            <a href="/receita5/exe1/movies.js" id={link} onClick={handler}> {url === '' ? 'Mostrar' : 'Ocultar'} </a>
+
+        </div>
+
+    )
+
+}
